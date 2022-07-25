d61ece381fc738734d2ad5e3925a9765cfcb6bb0
Shyam <yshyam787@gmail.com>
2022-07-25 20:58:07 +0200

    Initial Commit

---
+++ ADS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ADS
+//
+//  Created by Shyam Yadav on 25.07.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, Shyammmm!\n";
+    return 0;
+}
